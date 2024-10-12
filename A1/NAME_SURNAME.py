31fce84d4722e9258c8427f06140e71699fcae8f
rahulhirur <77510870+rahulhirur@users.noreply.github.com>
2024-10-12 15:59:10 +0200

    Add files via upload
    Assignment 1 dump

---
+++ A1/NAME_SURNAME.py
@@ -0,0 +1,35 @@
+'''
+Template for Assignment 1
+'''
+
+import numpy as np # Is it version 2.1 the one you are running?
+import matplotlib.pyplot as plt 
+import torch # Is it version 2.4 the one you are running?
+import torch.nn as nn
+import torch.optim as optim
+
+def plot_polynomial(coeffs, z_range, color='b'):
+    pass
+    
+
+def create_dataset(coeffs, z_range, sample_size, sigma, seed=42):
+    X = None
+    y = None
+    return X, y
+
+def visualize_data(X, y, coeffs, z_range, title=""):
+    pass
+
+
+if __name__ == "__main__":
+    '''
+    Code for Q1
+    '''
+    assert np.version.version=="2.1"
+
+    
+    '''
+    Code for Q2
+    '''
+    # Please continue organizing your script this way.
+    
